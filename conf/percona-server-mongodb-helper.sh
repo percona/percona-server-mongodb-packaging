63429973b5fc2bb0be3f7425a182e6d194ed7ce4
Alexey Bychko <alexey.bychko@percona.com>
2015-09-25 23:55:14 +0600

    BLD-332 added correct logic for THP

--- conf/percona-server-mongodb-helper.sh
+++ conf/percona-server-mongodb-helper.sh
@@ -5,11 +5,20 @@ PATH="${PATH}:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin"
 touch /var/run/mongod.pid
 chown mongod:mongod /var/run/mongod.pid
 #
-if [ -f /sys/kernel/mm/transparent_hugepage/enabled ]; then
-   echo never > /sys/kernel/mm/transparent_hugepage/enabled
-fi
+KTHP=/sys/kernel/mm/transparent_hugepage
 #
-if [ -f /sys/kernel/mm/transparent_hugepage/defrag ]; then
-	echo never > /sys/kernel/mm/transparent_hugepage/defrag
+[ -z "${CONF}" ] && CONF=/etc/mongod.conf
+
+print_error(){
+  echo " * Error disabling Transparent Huge pages, exiting"
+  exit 1
+}
+
+# checking if PerconaFT is used looking at defaults file and daemon config
+storageEngine=$( (egrep -o '^[[:blank:]]?storageEngine.*PerconaFT' ${CONF} || echo "${OPTIONS}" | egrep -o 'storageEngine.*PerconaFT' ) | tr -d '[[:blank:]]' | awk -F'=' '{print $NF}' 2>/dev/null)
+
+# trying to disable THP only if PerconaFT engine is enabled
+if [ "${storageEngine}" = PerconaFT ]; then
+  fgrep '[always]' ${KTHP}/enabled  2> /dev/null && (echo never > ${KTHP}/enabled 2> /dev/null || print_error)
+  fgrep '[always]' ${KTHP}/defrag 2> /dev/null && (echo never > ${KTHP}/defrag 2> /dev/null || print_error)
 fi
-#
