301ac25b424f712052b8f6d19463ec6f5ba4f752
Alexey Bychko <alexey.bychko@percona.com>
2015-12-10 15:07:09 +0600

    BLD-370 fixes for helper script

--- conf/percona-server-mongodb-helper.sh
+++ conf/percona-server-mongodb-helper.sh
@@ -8,17 +8,35 @@ chown mongod:mongod /var/run/mongod.pid
 KTHP=/sys/kernel/mm/transparent_hugepage
 #
 [ -z "${CONF}" ] && CONF=/etc/mongod.conf
-
+#
 print_error(){
   echo " * Error disabling Transparent Huge pages, exiting"
   exit 1
 }
-
 # checking if PerconaFT is used looking at defaults file and daemon config
-storageEngine=$( (egrep -o '^[[:blank:]]?storageEngine.*PerconaFT' ${CONF} || echo "${OPTIONS}" | egrep -o 'storageEngine.*PerconaFT' ) | tr -d '[[:blank:]]' | awk -F'=' '{print $NF}' 2>/dev/null)
-
-# trying to disable THP only if PerconaFT engine is enabled
+defaults=$(echo "${OPTIONS}" | egrep -o 'storageEngine.*PerconaFT' | tr -d '[[:blank:]]' | awk -F'=' '{print $NF}' 2>/dev/null)
+config=$(egrep -o '^[[:blank:]]+engine.*PerconaFT' ${CONF} | tr -d '[[:blank:]]' | awk -F':' '{print $NF}' 2>/dev/null)
+#
+if [ -z "${defaults}" ] && [ -z "${config}" ]; then # nothing to do
+  exit 0
+fi
+#
+if [ -n "${defaults}" ] && [ -n "${config}" ]; then # engine is set in 2 places
+  if [ "${defaults}" ==  "${config}" ]; then # it's OK
+    echo " * Warning, engine is set both in defaults file and mongod.conf!"
+  else
+    echo " * Error, different engines are set in the same time!"
+    exit 1
+  fi
+fi
+#
+if [ -n "${defaults}" ]; then
+  storageEngine=${defaults}
+else
+  storageEngine=${config}
+fi
+# trying to disable THP only if PerconaFT engine is enabled and THP is set to always
 if [ "${storageEngine}" = PerconaFT ]; then
-  fgrep '[always]' ${KTHP}/enabled  2> /dev/null && (echo never > ${KTHP}/enabled 2> /dev/null || print_error)
-  fgrep '[always]' ${KTHP}/defrag 2> /dev/null && (echo never > ${KTHP}/defrag 2> /dev/null || print_error)
+  fgrep '[always]' ${KTHP}/enabled  2> /dev/null && (echo never > ${KTHP}/enabled 2> /dev/null || print_error) || true
+  fgrep '[always]' ${KTHP}/defrag 2> /dev/null && (echo never > ${KTHP}/defrag 2> /dev/null || print_error) || true
 fi
